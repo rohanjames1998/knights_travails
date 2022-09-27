8189eb8acdcbc4ba4c83e34aa2a7919f33e5d911
Rohan James <rohanjames9282@gmail.com>
2022-09-27 17:06:18 +0530

    Remove #add_possible_moves method

--- lib/board.rb
+++ lib/board.rb
@@ -19,47 +19,7 @@ module Board_functions
     row
   end
 
-    # This method takes a 2d array and adds all possible moves a knight can make to each square.
-    def add_possible_moves(board)
-      board.each_with_index do |row, r_index|
-        row.each_with_index do |sq, index|
-          # For each square we first check if we can move above/below/right/left of the square.(the two steps)
-          # If we can then we check once more if we can move right/left from that position.(the half steps).
-            # Checking if we can move above.
-            if board[r_index + 2] && board[r_index + 2][index + 1]
-              sq.possible_moves << board[r_index + 2][index + 1]
-            end
-            if board[r_index + 2] && board[r_index + 2][index - 1] && index - 1 >= 0
-              sq.possible_moves << board[r_index + 2][index - 1]
-            end
-            # Checking if we can move right.
-            if board[r_index + 1] && board[r_index + 1][index + 2]
-              sq.possible_moves << board[r_index + 1][index + 2]
-            end
-            # When we subtract index number we have to check if we don't accidentally check for
-            # the last index as index can be negative. Hence the third statement
-            if board[r_index - 1] && board[r_index - 1][index + 2] && r_index - 1 >= 0
-              sq.possible_moves << board[r_index - 1][index + 2]
-            end
-            # Checking if we can move bottom.
-            if board[r_index - 2] && board[r_index - 2][index + 1] && r_index - 2 >= 0
-              sq.possible_moves << board[r_index - 2][index + 1]
-            end
-            # Here we have to make sure that both r_index and index are positive integers.
-            if board[r_index - 2] && board[r_index - 2][index - 1] && index - 1 >= 0 && r_index - 2 >= 0
-              sq.possible_moves << board[r_index - 2][index - 1]
-            end
-            # Checking if we can move left.
-            if board[r_index + 1] && board[r_index + 1][index + 2]
-              sq.possible_moves << board[r_index + 1][index + 2]
-            end
-            if board[r_index - 1] && board[r_index - 1][index + 2] && index - 1 >= 0 && r_index - 2 >= 0
-              sq.possible_moves << board[r_index - 1][index + 2]
-            end
-        end
-      end
-    end
-  end
+
 
 class Square
   # A square is a single position on the chess board where knight can be placed.
