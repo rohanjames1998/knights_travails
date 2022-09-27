fc01fec77cd43dbc25aa175012cd3bfbeb6f526f
Rohan James <rohanjames9282@gmail.com>
2022-09-27 14:31:01 +0530

    Change adjacent squares to possible moves knights can make

--- lib/board.rb
+++ lib/board.rb
@@ -10,7 +10,7 @@ module Board_functions
         sq = Square.new
         sq.position = [r_index, col_index]
         col_index += 1
-        col << sq.position
+        col << sq
       end
       row << col
       r_index += 1
@@ -19,25 +19,47 @@ module Board_functions
     row
   end
 
-    # This method takes a 2d array of 8x8 and adds adjacent squares for each square
-    # (i.e the elements of the array).
+    # This method takes a 2d array and adds all possible moves a knight can make to each square.
     def add_possible_moves(board)
       board.each_with_index do |row, r_index|
         row.each_with_index do |sq, index|
-          # For each square on the board we check if there is a square above it,
-          # after it, below it or before it. If there is one we add that as its
-          # adjacent square.
-          if r_index >= 1
-            sq.possible_moves << board[r_index - 1][index]
+          # For each square we first check if we can move above/below/right/left of the square.(the two steps)
+          # If we can then we check once more if we can move right/left from that position.(the half steps).
+          if board[r_index + 2]
+            # Checking if we can move above.
+            if board[r_index + 2][index + 1]
+            sq.possible_moves << board[r_index + 2][index + 1].position
+            end
+            if board[r_index + 2][index - 1]
+            sq.possible_moves << board[r_index + 2][index - 1].position
+            end
           end
-          if row[index + 1]
-            sq.possible_moves << row[index + 1]
+          if row[index + 2]
+            # Checking if we can move right.
+            if board[r_index + 1] && board[r_index + 1][index + 2]
+            sq.possible_moves << board[r_index + 1][index + 2].position
+            end
+            if board[r_index - 1] && board[r_index - 1][index + 2]
+              sq.possible_moves << board[r_index - 1][index + 2].position
+            end
           end
-          if board[r_index + 1]
-            sq.possible_moves << board[r_index + 1][index]
+          if board[r_index - 2]
+            # Checking if we can move bottom.
+            if board[r_index - 2][index + 1]
+            sq.possible_moves << board[r_index - 2][index + 1].position
+            end
+            if board[r_index - 2][index - 1]
+              sq.possible_moves << board[r_index - 2][index - 1].position
+            end
           end
-          if row[index - 1]
-            sq.possible_moves << row[index - 1]
+          if row[index - 2]
+            # Checking if we can move left.
+            if board[r_index + 1] && board[r_index + 1][index + 2]
+            sq.possible_moves << board[r_index + 1][index + 2].position
+            end
+            if board[r_index - 1] && board[r_index - 1][index + 2]
+              sq.possible_moves << board[r_index - 1][index + 2].position
+            end
           end
         end
       end
@@ -67,9 +89,9 @@ class Board
 
   def initialize
     @board = add_sq_to_board
-    # add_possible_moves(board)
+    add_possible_moves(board)
   end
 end
 
 b = Board.new
-p b.board
+p b.board[0][0].possible_moves
