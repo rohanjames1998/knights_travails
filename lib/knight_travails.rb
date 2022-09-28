0f910fcf9c01efc950a0a48431e6df9449326a71
Rohan James <rohanjames9282@gmail.com>
2022-09-28 10:35:28 +0530

    Add #knight_travails method

--- lib/knight_travails.rb
+++ lib/knight_travails.rb
@@ -1,3 +1,30 @@
 require_relative 'board.rb'
+require_relative 'queue.rb'
 
+def knight_travails(start, final_position)
+
+  queue = Queue.new
+  board = Board.new
+  visited_squares = {}
+  previous_squares = {}
+  queue.enqueue(start)
+  while queue.read
+    current_position = queue.dequeue
+    if !visited_squares[current_position]
+      visited_squares[current_position] = true
+      queue.enqueue_all_moves(board[current_position].possible_moves)
+      previous_squares[queue.read] = current_position
+    end
+  end
+  shortest_path = []
+  current_sq = final_position
+  while current_sq != start
+    shortest_path << current_sq
+    current_sq = previous_squares[current_sq]
+  end
+  shortest_path << start
+  return shortest_path.reverse
+end
+
+p knight_travails([3,3],[4,3])
 
--- lib/queue.rb
+++ lib/queue.rb
@@ -11,7 +11,18 @@ class Queue
   end
 
   def dequeue
-    data.unshift
+    data.shift
   end
 
+  def read
+    data.first
+  end
+
+  # This is a special method we create for enqueuing all the possible moves
+  # from a square
+  def enqueue_all_moves(moves)
+    moves.each do |move|
+      data << move.position
+    end
+  end
 end
